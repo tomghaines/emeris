5bf1649ba97eee92248f72df3a2d2ea5d7d55945
tomghaines <tomhaines001@gmail.com>
2024-11-06 22:41:10 +0000

    feat: integrate satelliteService with TLE data from apiService

---
+++ server/services/apiService.ts
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { convertTLEData } from './testservice';
+
+interface Satellite {
+  satelliteId: number;
+  name: string;
+  date: string;
+  line1: string;
+  line2: string;
+}
+
+interface SatelliteInfo {
+  longitudeDeg: number;
+  latitudeDeg: number;
+  height: number;
+  azimuth: number;
+  elevation: number;
+  rangeSat: number;
+  doppler: number;
+}
+
+export const getTLEs = async () => {
+  try {
+    const response = await axios.get<{ member: Satellite[] }>(
+      'https://tle.ivanstanojevic.me/api/tle'
+    );
+
+    const data = response.data.member.map((satellite) => {
+      const { line1, line2, satelliteId, name, date } = satellite;
+      let satelliteInfo = {};
+      try {
+        satelliteInfo = convertTLEData(line1, line2);
+      } catch (err) {
+        console.error(
+          `Error calculating SGEP4 for satellite ${satelliteId}:`,
+          err
+        );
+        return { satelliteId, name, date, err: 'SGP4 calculation failed' };
+      }
+      return {
+        satelliteId: satellite.satelliteId,
+        name: satellite.name,
+        date: satellite.date,
+        ...satelliteInfo,
+      };
+    });
+    return data;
+  } catch (err) {
+    console.error('Error fetching TLE data:', err);
+  }
+};
