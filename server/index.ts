9fe0dd9939babe4ed9bf7e0ec7a9cff1da60ae14
tomghaines <tomhaines001@gmail.com>
2024-11-07 11:44:06 +0000

    fix: install & use cors

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './router';
+import cors from 'cors';
 
 const port = 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
