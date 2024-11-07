ba62fa6f2e700c0f4499cc9366f06d8510186a5e
tomghaines <tomhaines001@gmail.com>
2024-11-07 13:30:06 +0000

    chore: create mock data and implement flag for switching data source - development purposes only

--- client/src/components/DisplayDataComponent/SatelliteData.tsx
+++ client/src/components/DisplayDataComponent/SatelliteData.tsx
@@ -1,41 +1,101 @@
 import { useEffect, useState } from 'react';
 import { getSatelliteData } from '../../services/satelliteAPI';
 
+interface Satellite {
+  satelliteId: number;
+  name: string;
+  date: string;
+  longitudeDeg: number;
+  latitudeDeg: number;
+  height: number;
+  azimuth: number;
+  elevation: number;
+  rangeSat: number;
+  doppler: number;
+}
+
+const mockData: Satellite[] = [
+  {
+    satelliteId: 25544,
+    name: 'ISS (ZARYA)',
+    date: '2024-11-07T08:50:54+00:00',
+    longitudeDeg: 0,
+    latitudeDeg: 0,
+    height: 0,
+    azimuth: 0,
+    elevation: 0,
+    rangeSat: 0,
+    doppler: 0,
+  },
+  {
+    satelliteId: 40075,
+    name: 'AISSAT 2',
+    date: '2023-12-28T11:59:02+00:00',
+    longitudeDeg: 0,
+    latitudeDeg: 0,
+    height: 0,
+    azimuth: 0,
+    elevation: 0,
+    rangeSat: 0,
+    doppler: 0,
+  },
+  {
+    satelliteId: 36797,
+    name: 'AISSAT 1',
+    date: '2024-11-06T14:26:09+00:00',
+    longitudeDeg: 0,
+    latitudeDeg: 0,
+    height: 0,
+    azimuth: 0,
+    elevation: 0,
+    rangeSat: 0,
+    doppler: 0,
+  },
+  {
+    satelliteId: 43694,
+    name: 'PROXIMA I',
+    date: '2024-05-18T00:08:26+00:00',
+    longitudeDeg: 0,
+    latitudeDeg: 0,
+    height: 0,
+    azimuth: 0,
+    elevation: 0,
+    rangeSat: 0,
+    doppler: 0,
+  },
+];
+
 const SatelliteData = () => {
-  interface Satellite {
-    satelliteId: number;
-    name: string;
-    date: string;
-    longitudeDeg: number;
-    latitudeDeg: number;
-    height: number;
-    azimuth: number;
-    elevation: number;
-    rangeSat: number;
-    doppler: number;
-  }
   const [satelliteData, setSateliteData] = useState<Satellite[]>([]);
   const [loading, setLoading] = useState(true);
+  const useMockData = true; // ! Change here to switch between api and mock data
 
   useEffect(() => {
     const fetchDataFromService = async () => {
-      try {
-        const response = await getSatelliteData();
-        if (response?.data) {
-          setSateliteData(response.data);
-          setLoading(false);
-        } else {
+      setLoading(true);
+
+      if (useMockData) {
+        setSateliteData(mockData);
+        setLoading(false);
+      } else {
+        try {
+          const response = await getSatelliteData();
+          if (response?.data) {
+            setSateliteData(response.data);
+            setLoading(false);
+          } else {
+            setSateliteData([]);
+            setLoading(false);
+          }
+        } catch (err) {
+          console.error('Error fetching satellite data:', err);
           setSateliteData([]);
-          setLoading(false);
+          setLoading(true);
         }
-      } catch (err) {
-        console.error('Error fetching satellite data:', err);
-        setSateliteData([]);
-        setLoading(true);
       }
     };
     fetchDataFromService();
-  }, []);
+  }, [useMockData]);
 
   if (loading) return <p>Loading data</p>;
 
