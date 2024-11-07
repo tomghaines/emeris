cd7c5c99497c61854168d20e5d3df02c95eebf5f
tomghaines <tomhaines001@gmail.com>
2024-11-07 12:27:31 +0000

    feat: create api service to retrieve data from server and render in data component

--- client/src/components/DisplayDataComponent/SatelliteData.tsx
+++ client/src/components/DisplayDataComponent/SatelliteData.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { getSatelliteData } from '../../services/satelliteAPI';
 
 const SatelliteData = () => {
-  return <div>This is the satellite data</div>;
+  const [satelliteData, setSateliteData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchDataFromService = async () => {
+      try {
+        const response = await getSatelliteData();
+        console.log(response.data);
+        setSateliteData(response.data);
+        setLoading(false);
+      } catch (err) {
+        console.error('Error fetching satellite data from the service', err);
+        setLoading(true);
+      }
+    };
+    fetchDataFromService();
+  }, []);
+
+  if (loading) return <p>Loading data</p>;
+
+  return (
+    <div>
+      {satelliteData &&
+        satelliteData.map((satellite) => (
+          <div key={satellite.satelliteId}>
+            <h3>Name: {satellite.name}</h3>
+            <p>ID: {satellite.satelliteId}</p>
+            <p>Date: {satellite.date}</p>
+            <p>Longitude: {satellite.longitudeDeg}</p>
+            <p>Latitude: {satellite.latitudeDeg}</p>
+            <p>Height: {satellite.height}</p>
+            <p>Elevation: {satellite.elevation}</p>
+            <p>Doppler Factor: {satellite.doppler}</p>
+            <p>Azimuth: {satellite.azimuth}</p>
+            <p>Range: {satellite.rangeSat}</p>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default SatelliteData;
---
+++ client/src/services/satelliteAPI.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const getSatelliteData = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000');
+    return response;
+  } catch (err) {
+    console.error('Error fetching satellite data from API', err);
+  }
+};
