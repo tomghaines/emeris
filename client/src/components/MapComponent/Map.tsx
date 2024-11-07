a274965ae9d7cda6624dde330b099192cb85a377
tomghaines <tomhaines001@gmail.com>
2024-11-07 19:47:42 +0000

    feat: add basic leaflet map with placeholder markers

--- client/src/components/MapComponent/Map.tsx
+++ client/src/components/MapComponent/Map.tsx
@@ -1,7 +1,34 @@
-import { MapContainer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
 const Map = () => {
-  return <MapContainer center={[48.8566, 2.3522]} zoom={10}></MapContainer>; // change these coords to centre of map
+  const markers = [
+    {
+      geocode: [-12.183, 60.169],
+      popUp: 'popup',
+    },
+    {
+      geocode: [48.98, -30.787],
+      popUp: 'popup',
+    },
+    {
+      geocode: [-15.255, -14.047],
+      popUp: 'popup',
+    },
+  ];
+
+  return (
+    <MapContainer center={[48.8566, 2.3522]} zoom={2}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'}
+      />
+      {markers.map((marker) => (
+        <Marker position={marker.geocode}></Marker>
+      ))}
+    </MapContainer>
+  ); // change these coords to centre of map
 };
 
 export default Map;
