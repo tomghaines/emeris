d5deb1c373c18322b5896ade7d03a94f207ae33f
tomghaines <tomhaines001@gmail.com>
2024-11-09 22:53:17 +0000

    feat: add lastUpdateTimestamp to controller to set newDate() or find last updated date

--- server/controllers/satelliteController.ts
+++ server/controllers/satelliteController.ts
@@ -5,7 +5,9 @@ import { fetchAndSaveTLEData } from '../services/apiService';
 export const getSatellites = async (req: Request, res: Response) => {
   try {
     const satellites = await Satellite.find().exec();
-    res.status(200).json(satellites);
+    const lastUpdateTimeStamp =
+      satellites.length > 0 ? satellites[0].lastUpdateTimestamp : new Date();
+    res.status(200).json({ satellites, lastUpdateTimeStamp });
   } catch (err) {
     console.error('Error retrieving satellite data:', err);
     res.status(500).json({ message: 'Error fetching satellite data' });
