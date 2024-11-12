da430f3b8e182167563ab754d45390cdaa1617aa
tomghaines <tomhaines001@gmail.com>
2024-11-12 16:52:59 +0000

    feat: update data parsing logic & clean up comments

--- client/src/components/SatelliteData.tsx
+++ client/src/components/SatelliteData.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
-import Map from './MapComponent/Map';
+import Map from './Map/Map';
 import { getSatelliteData } from '../services/satelliteAPI';
 import StatusBar from './StatusBar';
 import SideBar from './SideBarComponent/SideBar';
 import getSimulatedPosition from '../services/Simulation';
+import debounce from 'lodash.debounce';
 
 interface Satellite {
   _id: string;
@@ -19,12 +20,13 @@ interface Satellite {
   elevation: number;
   rangeSat: number;
   doppler: number;
-
   lastUpdateTimestamp: string;
 }
 
 const SatelliteData = () => {
-  const satelliteRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
+  const debouncedUpdateMarkerPositions = debounce((satellites: Satellite[]) => {
+    updateMarkerPositions(satellites);
+  }, 100);
   const [selectedSatelliteId, setSelectedSatelliteId] = useState<string | null>(
     null
   );
@@ -34,7 +36,37 @@ const SatelliteData = () => {
   const [lastUpdateTimestamp, setLastUpdateTimestamp] = useState<string | null>(
     null
   );
+  const [trackedSatellites, setTrackedSatellites] = useState<string[]>([]);
+  const [trackedSatelliteCount, setTrackedSatelliteCount] = useState(0);
+  const satelliteRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
+  const mapRef = useRef<L.Map | null>(null);
+  const markerRefs = useRef<{ [key: string]: L.Marker | null }>({});
+
+  const handleTrackSatellite = (id: string) => {
+    setTrackedSatellites((prevTracked) => {
+      const isAlreadyTracked = prevTracked.includes(id);
+      let updatedTrackedSatellites;
+
+      if (isAlreadyTracked) {
+        updatedTrackedSatellites = prevTracked.filter((satId) => satId !== id);
+      } else {
+        updatedTrackedSatellites = [...prevTracked, id];
+      }
+
+      // Log the satellites after updating state
+      // console.log('Updated Tracked Satellites:', updatedTrackedSatellites);
+      setTrackedSatelliteCount(updatedTrackedSatellites.length);
+
+      return updatedTrackedSatellites;
+    });
+  };
+
   const useMockData = false;
+
+  useEffect(() => {
+    // Log when tracked satellites are updated
+    // console.log('Tracked Satellites Updated:', trackedSatellites);
+  }, [trackedSatellites]);
 
   useEffect(() => {
     const fetchDataFromService = async () => {
@@ -44,8 +76,7 @@ const SatelliteData = () => {
         const satelliteArray = response?.data?.satellites || [];
         setSatelliteData(satelliteArray);
         setLastUpdateTimestamp(response?.data?.lastUpdateTimestamp || null);
-
-        console.log('Fetched Satellite Data:', satelliteArray);
+        // console.log('Fetched Satellite Data:', satelliteArray);
       } catch (err) {
         console.error('Error fetching satellite data:', err);
       } finally {
@@ -56,11 +87,23 @@ const SatelliteData = () => {
     fetchDataFromService();
   }, [useMockData]);
 
+  const updateMarkerPositions = (satellites: Satellite[]) => {
+    satellites.forEach((satellite) => {
+      const marker = markerRefs.current[satellite._id];
+      if (marker) {
+        marker.setLatLng([
+          satellite.latitudeDeg || 0,
+          satellite.longitudeDeg || 0,
+        ]);
+      }
+    });
+  };
+
   useEffect(() => {
     if (!lastUpdateTimestamp || satelliteData.length === 0) return;
 
     const interval = setInterval(() => {
-      setSimulatedData((prevData) => {
+      setSimulatedData(() => {
         const updatedData = satelliteData.map((satellite) => {
           const simulatedPosition = getSimulatedPosition(
             satellite,
@@ -68,24 +111,26 @@ const SatelliteData = () => {
           );
           return {
             ...satellite,
-            latitudeDeg: simulatedPosition.latitude,
-            longitudeDeg: simulatedPosition.longitude,
-            height: simulatedPosition.height,
-            velocity: simulatedPosition.velocity,
-            elevation: simulatedPosition.elevation,
-            azimuth: simulatedPosition.azimuth,
-            doppler: simulatedPosition.doppler,
-            rangeSat: simulatedPosition.rangeSat,
-            heading: simulatedPosition.heading,
+            latitudeDeg: simulatedPosition?.latitude,
+            longitudeDeg: simulatedPosition?.longitude,
+            height: simulatedPosition?.height,
+            velocity: simulatedPosition?.velocity,
+            elevation: simulatedPosition?.elevation,
+            azimuth: simulatedPosition?.azimuth,
+            doppler: simulatedPosition?.doppler,
+            rangeSat: simulatedPosition?.rangeSat,
+            heading: simulatedPosition?.heading,
           };
         });
-        console.log('Simulated Data After Update:', updatedData);
+
+        debouncedUpdateMarkerPositions(updatedData);
+        // console.log('Simulated Data After Update:', updatedData);
         return updatedData;
       });
-    }, 1500);
+    }, 1000);
 
     return () => clearInterval(interval);
-  }, [satelliteData, lastUpdateTimestamp]);
+  }, [satelliteData, lastUpdateTimestamp, debouncedUpdateMarkerPositions]);
 
   if (loading || satelliteData.length === 0) {
     return (
@@ -96,33 +141,40 @@ const SatelliteData = () => {
       </div>
     );
   }
-
   return (
-    <div className="flex h-screen p-2 gap-2  bg-[#0e0e0e]">
+    <div className="flex h-screen p-2 gap-2 bg-[#02050A]">
       <div className="flex flex-col h-full w-3/4 relative">
-        <div className="top-0 z-10 absolute m-3 rounded-md">
-          <StatusBar satelliteData={satelliteData} loading={loading} />
+        <div className="top-0 z-10 absolute m-2">
+          <StatusBar
+            satelliteData={satelliteData}
+            loading={loading}
+            trackedSatelliteCount={trackedSatelliteCount}
+            trackedSatellites={trackedSatellites}
+          />
         </div>
-        <div className="h-full z-0 overflow-hidden p-0 bg-[#02050A] rounded-lg">
+        <div className="h-full z-0 overflow-hidden rounded-md">
           <Map
             satelliteData={simulatedSatelliteData}
             loading={loading}
             selectedSatelliteId={selectedSatelliteId}
             onSatelliteSelect={setSelectedSatelliteId}
-            satelliteRefs={satelliteRefs}
+            trackedSatellites={trackedSatellites}
+            ref={mapRef}
+            markerRefs={markerRefs}
           />
         </div>
       </div>
-      <div
-        className="w-1/4 overflow-hidden p-0 bg-[#02050A] rounded-lg
-      "
-      >
+      <div className="w-1/4 overflow-hidden p-0 bg-[#02050A]">
         <SideBar
           satelliteData={simulatedSatelliteData}
           loading={loading}
           selectedSatelliteId={selectedSatelliteId}
           onSatelliteSelect={setSelectedSatelliteId}
           satelliteRefs={satelliteRefs}
+          handleTrackSatellite={handleTrackSatellite}
+          trackedSatellites={trackedSatellites}
+          mapRef={mapRef}
+          markerRefs={markerRefs}
         />
       </div>
     </div>
